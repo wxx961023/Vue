5b20cb0b817c8d430a79e9426e149720c11104f6
Brazen <764146802@qq.com>
2019-05-02 23:25:32 +0800

    完成两个组件

--- Vue.js 技术栈搭建的CNODE社区/Vuejswepack/axiosapp/src/components/HelloWorld.vue
+++ Vue.js 技术栈搭建的CNODE社区/Vuejswepack/axiosapp/src/components/HelloWorld.vue
@@ -1,94 +1,38 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
+    <h3>我说axiosApp，用来发送请求，拦截响应</h3>
+    <button @click="getData">点我发送请求</button>
+    <ul >
+      <li  :key="key" v-for="(item,key) in items">
+        {{item.title}}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Vue from 'vue'
+
+Vue.prototype.$http = axios
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      items:[]
+    }
+  },
+  methods:{
+    getData(){
+      this.$http.get('https://cnodejs.org/api/v1/topics')
+        .then((res)=>{
+          this.items = res.data.data
+          console.log(res.data.data)
+        })
+        .catch(function(err){
+          console.log(err)
+        })
     }
   }
 }
---
+++ Vue.js 技术栈搭建的CNODE社区/Vuejswepack/cnode/cnode/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <PostList></PostList>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import PostList from './components/PostList'
+export default {
+  name: 'App',
+  components:{
+    Header,
+    PostList
+  },
+}
+</script>
+
+<style>
+.main{
+  width: 80%;
+  margin: 0 auto; 
+}
+body {
+    background-color: #e1e1e1;
+    font-family: "Helvetica Neue","Luxi Sans","DejaVu Sans",Tahoma,"Hiragino Sans GB",STHeiti,sans-serif!important;
+}
+</style>
